45b15836db4f5c3909cbaf85760a67dd59af1bba
Thomas Boerger <thomas@webhippie.de>
2020-02-24 11:19:21 +0100

    Add more tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -16,3 +16,7 @@ def test_running_and_enabled(host):
     svc = host.service("mariadb")
     assert svc.is_running
     assert svc.is_enabled
+
+
+def test_server_socket(host):
+    assert host.socket("tcp://127.0.0.1:3306").is_listening
